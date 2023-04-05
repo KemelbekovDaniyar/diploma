507a748d951d69d8e7308de80eadb138156651ca
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-04-05 17:10:49 +0600

    added logo to header

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import Nav from "../Nav/Nav";
 import "./Layout.css";
+import Logo from "../../assets/logo.png";
 
 
 export default function Layout(props) {
   return (
     <div className="Layout">
       <header>
+        <img src={Logo} alt="logo" />
         <Nav />
       </header>
       <aside>
