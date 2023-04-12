fb682594f2fff1a410909846df15e51a8e4310fc
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-04-12 15:31:49 +0600

    Move db records to App component

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,17 +1,12 @@
 import { useMatch } from "react-router";
 import NotFound from "./NotFound";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export default function Category() {
   const { params } = useMatch("/categories/:slug");
 
-  const categories = [
-    { id: 1, name: "Guitars", slug: "guitars" },
-    { id: 2, name: "Audio & Recording", slug: "audio-recording" },
-    { id: 3, name: "Accessories", slug: "accessories" },
-    { id: 4, name: "Keyboards and pianos", slug: "keyboards-pianos" },
-    { id: 5, name: "Orchestral Instruments", slug: "orchestral-instruments" },
-    { id: 6, name: "Drums and percussion", slug: "drums-percussion" },
-  ];
+  const { categories } = useContext(AppContext);
 
   const category = categories.find(
     (category) => category.slug === params.slug
