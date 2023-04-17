1ea32688738dab8690e6b8612c147a9b8add1c36
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-04-17 15:53:28 +0600

    added a function when you press input the number will increase

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -6,6 +6,13 @@ import "./CartList.css"
 export default function CartList() {
   const { products, cart, setCart } = useContext(AppContext);
 
+  function onQuantityChange(product, qty) {
+    setCart({
+      ...cart,
+      [product.id]: qty,
+    });
+  }
+
   const productIds = Object.keys(cart);
 
   const output = products
@@ -14,7 +21,12 @@ export default function CartList() {
       <div className="CartItem" key={product.id}>
         <img src={product.picture} alt={product.name} />
         <Link to={`/products/` + product.slug}> {product.name}</Link>
-        <input type="number" value={cart[product.id]} min={1} />
+        <input
+          type="number"
+          value={cart[product.id]}
+          min={1}
+          onChange={(event) => onQuantityChange
+          (product, +event.target.value)} />
         <span>{cart[product.id] * product.price} $</span>
         <button>Remove</button>
       </div>
