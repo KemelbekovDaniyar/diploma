b849aa99ad9e998ec8491ae90954d07752a03886
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-04-22 23:25:33 +0600

    new code

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,28 +3,28 @@ import './Carousel.css';
 import Img1 from '../../assets/q1w.png';
 import Img2 from '../../assets/ElectricGuitar.png';
 
-function Carousel({ images }) {
+function Carousel({ image }) {
 
   const imagess = [
     Img1,
     Img2,
   ];
 
-  images = imagess;
+  image = imagess;
 
   const [index, setIndex] = useState(0);
 
   function handlePrev() {
-    setIndex(prevIndex => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setIndex(prevIndex => (prevIndex === 0 ? image.length - 1 : prevIndex - 1));
   }
 
   function handleNext() {
-    setIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setIndex(prevIndex => (prevIndex === image.length - 1 ? 0 : prevIndex + 1));
   }
 
   return (
     <div className="Carousel">
-      <img src={images[index]} alt={`Image ${index + 1}`} />
+      <img src={image[index]} alt="carousel" />
       <div className='btn'>
         <button onClick={handlePrev}>Prev</button>
         <button onClick={handleNext}>Next</button>
