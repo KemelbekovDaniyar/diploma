77dd61483385b88a5f33937f7603497c0427cbab
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-04-25 15:53:14 +0600

    delete code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,7 @@
-import guitar from '../assets/ElectricGuitar.png';
-import drum from '../assets/q1w.png';
-import piano from '../assets/Piano.png';
-
 export default function Home() {
   return (
     <div className="Home">
-      <div className="stocks">
-        <div className="guitar">
-          <div className="guitar__box">
-            <div className="guitar__title">
-              <p>Fender American</p>
-              <span>Only: </span>
-              <span>999$</span>
-              <button>Buy</button>
-            </div>
-            <img src={guitar} alt="guitar" />
-          </div>
-        </div>
-        <div className="drum">
-          <div className="drum__box">
-            <img src={drum} alt="drum" />
-            <div className="drum__title">
-              <p>Yamaha</p>
-              <span>Only: </span>
-              <span>999$</span>
-              <button>Buy</button>
-            </div>
-          </div>
-        </div>
-        <div className="piano">
-          <div className="piano__box">
-            <div className="piano__title">
-              <p>Yamaha</p>
-              <span>Only: </span>
-              <span>999$</span>
-              <button>Buy</button>
-            </div>
-            <img src={piano} alt="piano" />
-          </div>
-        </div>
-      </div>
+      <h1>Home</h1>
     </div>
   );
 }
