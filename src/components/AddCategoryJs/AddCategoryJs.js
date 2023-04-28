ed10db239a99c81c64d54146aadd008b5117188f
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-04-28 14:40:45 +0600

    add category to firestore

--- src/components/AddCategoryJs/AddCategoryJs.js
+++ src/components/AddCategoryJs/AddCategoryJs.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import './AddCategoryJs.css'
+import { addDoc } from "firebase/firestore/lite"
+import { categoryCollection } from "../../firebase"
 
 export default function AddCategoryJs() {
   const [category, setCategory] = useState('')
@@ -9,18 +11,32 @@ export default function AddCategoryJs() {
   }
 
   function onAddCategory() {
-    
+    const name = category.trim();
+
+    if (name.length < 5) {
+      alert('Category name must be longer than 5 characters')
+      return
+    }
+
+    addDoc(categoryCollection, {
+      name: name,
+      slug: name
+        .replace(" ", '-')
+        .toLowerCase(),
+    }).then(() => {
+      setCategory('')
+    })
   }
 
 
 
   return (
     <div className="AddCategoryJs">
-      <input 
-      type="text" 
-      placeholder="Category name" 
-      onChange={onChangeCategory} 
-      value={category} />
+      <input
+        type="text"
+        placeholder="Category name"
+        onChange={onChangeCategory}
+        value={category} />
       <button onClick={onAddCategory}>Add category</button>
     </div>
   )
