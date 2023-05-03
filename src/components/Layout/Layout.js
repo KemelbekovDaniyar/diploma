5bb0ab63469c39cb9f36ea8b5d55d9bf9b485176
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-05-03 14:38:38 +0600

    made adaptability to navigation and fixed errors

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,16 +4,28 @@ import Logo from "../Logo/Logo";
 import CategoryList from "../CategoryList/CategoryList";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
+import { useState } from "react";
+import Drawer from "../Drawer/Drawer";
+import NavToggle from "../NavToggle/NavToggle";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
         <div className="spacer">
+          <NavToggle callback={toggleDrawer} />
           <Logo />
           <Nav />
-          <CartLink />
-          <Auth />
+          <Drawer open={drawerOpen} toggle={toggleDrawer} />
+          <div>
+            <CartLink />
+            <Auth />
+          </div>
         </div>
       </header >
       <aside>
