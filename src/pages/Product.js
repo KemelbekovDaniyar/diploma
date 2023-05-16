f7cf21cf02a7b98915261d84e88ed6c42fe33157
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-05-16 14:28:41 +0600

    set a new style for the product

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -11,17 +11,20 @@ export default function Product() {
   const product = products.find(product => product.slug === params.slug);
 
   if (!product) {
-    return <NotFound />  
+    return <NotFound />
   }
 
   return (
     <div className="Product">
-      <h1>{product.name}</h1>
-      <img src={product.picture} alt={product.name} />
-      <span>{product.price}$</span>
-      <AddToCart product={product} />
-      <p className="info">{product.info}</p>
-      
+      <div className="hero">
+        <h1>{product.name}</h1>
+        <img src={product.picture} alt={product.name} />
+        <span>{product.price}$</span>
+        <AddToCart product={product} />
+      </div>
+      <div className="info">
+        <p className="info">{product.info}</p>
+      </div>
     </div>
   )
 }
