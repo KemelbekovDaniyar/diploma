4265c9fa041ac50ca9d35c6584e38cdbd2abd69c
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-05-16 14:55:36 +0600

    created the layout of the future component

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,7 +1,15 @@
+import "./Music.css"
+
+
 export default function Music() {
   return (
     <div className="Music">
       <h1>Music</h1>
+      <div className="Music-container">
+        <div className="info"></div>
+        <div className="play"></div>
+        <div className="who"></div>
+      </div>  
     </div>
   )
 }
