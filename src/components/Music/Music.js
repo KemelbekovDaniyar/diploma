a29386ebc26fcb9c225b22a35cc1831875b5466d
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-05-18 12:57:28 +0600

    add background to blocks and hover

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,4 +1,7 @@
 import "./Music.css"
+import img from "../../assets/whayguitars.jpg"
+import img1 from "../../assets/string.jpg"
+import img2 from "../../assets/children.jpg"
 
 
 export default function Music() {
@@ -6,10 +9,43 @@ export default function Music() {
     <div className="Music container">
       <h1>Music</h1>
       <div className="Music-container">
-        <div className="info"></div>
-        <div className="play"></div>
-        <div className="who"></div>
-      </div>  
+        <div className="info">
+          <ul class="hover-effect-scale">
+            <li>
+              <img src={img} alt="" />
+                <div>
+                  <h3>Заголовок</h3>
+                  <p>Текст</p>
+                  <a href="">Подробнее</a>
+                </div>
+            </li>
+          </ul>
+        </div>
+        <div className="play">
+        <ul class="hover-effect-scale">
+            <li>
+              <img src={img1} alt="" />
+                <div>
+                  <h3>Заголовок</h3>
+                  <p>Текст</p>
+                  <a href="">Подробнее</a>
+                </div>
+            </li>
+          </ul>
+        </div>
+        <div className="who">
+        <ul class="hover-effect-scale">
+            <li>
+              <img src={img2} alt="" />
+                <div>
+                  <h3>Заголовок</h3>
+                  <p>Текст</p>
+                  <a href="">Подробнее</a>
+                </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
