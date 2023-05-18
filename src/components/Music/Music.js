7f1c701c433393bfb0205e84d4f9fc11aca61fd9
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-05-18 14:13:31 +0600

    changed the style of the blocks and added a new code

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,7 +1,6 @@
 import "./Music.css"
 import img from "../../assets/whayguitars.jpg"
 import img1 from "../../assets/string.jpg"
-import img2 from "../../assets/children.jpg"
 
 
 export default function Music() {
@@ -10,40 +9,26 @@ export default function Music() {
       <h1>Music</h1>
       <div className="Music-container">
         <div className="info">
-          <ul class="hover-effect-scale">
-            <li>
+          <div class="hover-text-one">
+            <figure class="effect-text-one">
               <img src={img} alt="" />
-                <div>
-                  <h3>Заголовок</h3>
-                  <p>Текст</p>
-                  <a href="">Подробнее</a>
-                </div>
-            </li>
-          </ul>
+              <figcaption>
+                <h3>Lorem <span>ipsum</span></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              </figcaption>
+            </figure>
+          </div>
         </div>
         <div className="play">
-        <ul class="hover-effect-scale">
-            <li>
+          <div class="hover-text-one">
+            <figure class="effect-text-one">
               <img src={img1} alt="" />
-                <div>
-                  <h3>Заголовок</h3>
-                  <p>Текст</p>
-                  <a href="">Подробнее</a>
-                </div>
-            </li>
-          </ul>
-        </div>
-        <div className="who">
-        <ul class="hover-effect-scale">
-            <li>
-              <img src={img2} alt="" />
-                <div>
-                  <h3>Заголовок</h3>
-                  <p>Текст</p>
-                  <a href="">Подробнее</a>
-                </div>
-            </li>
-          </ul>
+              <figcaption>
+                <h3>Lorem <span>ipsum</span></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              </figcaption>
+            </figure>
+          </div>
         </div>
       </div>
     </div>
