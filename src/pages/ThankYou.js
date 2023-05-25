9712d8bdf56722e68aebc9ecd22cef92ff84618c
Daniyar Kemelbekov <kemelbekovdaniyar0@gmail.com>
2023-05-25 14:21:42 +0600

    downloaded the picture and styled the page thank you

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,9 +1,12 @@
+import img from "./../assets/thank-you.png"
+
 export default function ThankYou() {
-  setTimeout(() => {
-    window.location.href = "/";
-  }, 2000);
+  // setTimeout(() => {
+  //   window.location.href = "/";
+  // }, 2000);
   return (
     <div className="ThankYou">
+      <img src={img} alt="thank-you" />
       <h1>Thank you!</h1>
       <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
     </div>
